6658aa106f1f2cd960088606407420480d9fa4f9
Thuve <thuverakantharma@gmail.com>
2025-09-20 21:30:57 +0530

    testing ci/cd

--- app/loginmain/page.js
+++ app/loginmain/page.js
@@ -21,7 +21,7 @@ export default function LoginMain() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-center text-gray-900">Login</h1>
+        <h1 className="text-2xl font-bold text-center text-gray-900">Welcome</h1>
         <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
